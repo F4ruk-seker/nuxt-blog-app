b208fd65adadb2c19c5284f0f22b6486ff06683b
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-04-14 15:10:46 +0300

    add btn

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-    pars
+    <section class=" h-screen">
+        <section class="container m-auto flex border-2 h-screen bg-blend-lighten hover:bg-blend-darken">
+            <article class="w-3/12 ">
+                <h1 class="bg-orange-600 text-white font-bold text-center py-2">BLOG</h1>
+                <ul class="list-none">
+                    <li>
+                        <button class="bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 rounded-0 w-full">
+                            Tag 1
+                        </button>
+                    </li>
+                    <li>Tag 2</li>
+                    <li>Tag 3</li>
+                    <li>Tag 4</li>
+                    <li>Tag 5</li>
+                </ul>
+            </article>
+        </section>
+    </section>
+    
 </template>

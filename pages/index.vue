965d9e17de00e0a694f136346e0ff17259ed18f2
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-04-14 21:10:17 +0300

    add responsive menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,33 @@
 <template>
     <section class=" h-screen">
-        <section class="container m-auto flex border-2 h-screen bg-blend-lighten hover:bg-blend-darken">
-            <article class="w-3/12 ">
-                <h1 class="bg-orange-600 text-white font-bold text-center py-2">BLOG</h1>
+        <section class="w-full xl:container m-auto flex h-screen bg-blend-lighten hover:bg-blend-darken static">
+            <article class="w-full md:w-3/12 absolute md:static">
                 <ul class="list-none">
-                    <li>
-                        <button class="bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 rounded-0 w-full">
-                            Tag 1
+                    <li class="flex w-full h-full relative">
+                        <h1 class="bg-white md:bg-orange-600 text-orange-600 md:text-white  font-bold text-center py-2 w-f w-full">BLOG</h1>
+                        <button class="bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 h-full font-bold border-2 px-2 z-10 border-white rounded-0 absolute right-0 md:hidden" @click="modal_menu=!modal_menu">
+                            <font-awesome-icon :icon="modal_menu ?  ['fas', 'bars']:['fas', 'x']" />
                         </button>
                     </li>
-                    <li>Tag 2</li>
-                    <li>Tag 3</li>
-                    <li>Tag 4</li>
-                    <li>Tag 5</li>
+                    <ul :class=" (modal_menu ? 'hidden':'') + ' md:block ' ">
+                        <li>
+                            <button class="bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 border-2 border-white rounded-0 w-full">
+                                Tag 22
+                            </button>
+                        </li>
+                    </ul>
                 </ul>
+            </article>
+            <article class="w-full mt-10 md:mt-0">
+                <hr>                
             </article>
         </section>
     </section>
     
 </template>
+
+<script setup>
+const modal_menu = ref(true)
+
+// + (modal_menu ? 'show':'hidden')"
+</script>

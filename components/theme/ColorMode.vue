681c3aa7749ba6532b6ab6e0d78ce1cd079f8bd5
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-04-15 13:14:16 +0300

    add ColorMode New View

--- components/theme/ColorMode.vue
+++ components/theme/ColorMode.vue
@@ -1,43 +1,123 @@
 <template>
-    <div class="flex items-center justify-center">
-    <select v-model="$colorMode.preference" class="select-color-mode w-full appearance-none border border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-      <option value="sepia">Sepia</option>
-    </select>
+  <div class="relative">
+    <div class="flex justify-between m-2 md:w-1/2 md:rounded-full border-2 border-y-0 m-auto py-5">
+      <button class="border-0 p-3 hover:text-orange-400 " :disabled="index==0" @click="decrease">
+        <font-awesome-icon :icon="['fas', 'arrow-left']" />
+      </button>
+      <div class="m-3 shadow-lg">
+        <Transition name="slide-fade">
+          <span v-if="colorMode.preference === 'system'"><font-awesome-icon  :icon="['fas', 'wand-sparkles']" /></span>
+        </Transition>
+        <Transition name="slide-fade">
+          <span v-if="colorMode.preference === 'light'" ><font-awesome-icon :icon="['fas', 'sun']" /></span>
+        </Transition>
+        <Transition name="slide-fade">
+          <span v-if="colorMode.preference === 'dark'"  ><font-awesome-icon :icon="['fas', 'moon']" /></span>
+        </Transition>
+        <Transition name="slide-fade">
+          <span v-if="colorMode.preference === 'sepia'" ><font-awesome-icon :icon="['fas', 'mug-hot']" /></span>
+        </Transition>
+      </div>
+      <button class="border-0 p-3 hover:text-orange-400" :disabled="options.length-1 == index" @click="increase">
+        <font-awesome-icon :icon="['fas', 'arrow-right'] " />
+      </button>
+    </div>
+    <span class="absolute mx-auto text-center w-full bottom-0 mb-2"> {{ colorMode.preference }} </span>
+
   </div>
-  </template>
+</template>
   
-  <script setup>
-  const colorMode = useColorMode()
-  // {{ $colorMode.value }}
-  console.log(colorMode.preference)
-  </script>
-  
-  <style>
+<script setup>
+const index = ref(0)
+const options = ref([
+  {
+    value: 'system',
+    icon: 'wand-sparkles'
+  },
+  {
+    value: 'light',
+    icon: 'sun'
+  },
+  {
+    value: 'dark',
+    icon: 'moon'
+  },
+  {
+    value: 'sepia',
+    icon: 'mug-hot'
+  }
+])
+
+function increase() {
+  if (options.value.length - 1 > index.value) {
+    index.value++;
+    set_icon()
+  }
+}
+
+function decrease() {
+  if (index.value - 1 >= 0) {
+    index.value--;
+    set_icon()
+  }
+}
+
+function set_icon() {
+  colorMode.preference = options.value[index.value].value
+}
+
+const colorMode = useColorMode()
+
+
+
+onBeforeUpdate(()=>{
+  for (let _ = 0; _ < options.value.length; _++) {
+    if (options.value[_].value === colorMode.preference){
+      index.value = _
+    }
+  }
+})
+
+</script>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
+
+<style>
+
+.select-color-mode{
+  @apply text-orange-400
+}
+
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
 
   .select-color-mode{
-    @apply text-orange-400
+    @apply text-teal-600
   }
 
-  body {
-    background-color: #fff;
-    color: rgba(0,0,0,0.8);
-  }
-  .dark-mode body {
-    background-color: #091a28;
-    color: #ebf4f1;
-
-    .select-color-mode{
-      @apply text-teal-600
-    }
-
-  }
-  .sepia-mode body {
-    background-color: #f1e7d0;
-    color: #433422;
-  }
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
 
 </style>
-  
--- pages/index.vue
+++ pages/index.vue
@@ -16,12 +16,23 @@
                                     Tag 22
                                 </button>
                             </li>
+                            <li>
+                                <button class="dark:bg-transparent dark:border-0 dark:hover:bg-gray-900 dark:text-orange-500 bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 border-2 border-white rounded-0 w-full">
+                                    Tag 22
+                                </button>
+                            </li>
+                            <li>
+                                <button class="dark:bg-transparent dark:border-0 dark:hover:bg-gray-900 dark:text-orange-500 bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 border-2 border-white rounded-0 w-full">
+                                    Tag 22
+                                </button>
+                            </li>
+                            <li><hr></li>
+                            <li class="mt-4">
+                                <ColorMode />
+                            </li>
                         </ul>
                     </li>
-                    <li><hr></li>
-                    <li>
-                        <ColorMode />
-                    </li>
+                    
                 </ul>
             </article>
             <article class="w-full mt-10 md:mt-0">

8be681ff3e4081c190781b6cff228c8a04b78a0a
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-04-16 22:06:04 +0300

    add Header

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full">
         <input type="text" 
-        class="caret-orange-500 border-0 outline-none bg-gray-50 focus:border-0 active:border-0 border-gray-300 text-orange-700 text-sm font-bold focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-        placeholder="search" />
+        class="caret-orange-500 rounded-2xl border focus:ring-blue-500 focus:border-gray-600  outline-none bg-gray-100 dark:focus:border-0 dark:active:border-0 border-gray-300 text-orange-700 text-sm font-bold block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+        placeholder=">_ search" />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,17 @@
 <template>
-    <section class="max-h-screen">
-        <section class="w-full m-auto flex h-screen bg-blend-lighten hover:bg-blend-darken static ">
-            <article class="w-full md:w-4/12 absolute md:static border-0 border-e border-gray-100 z-10">
-                <ul class="list-none">
-                    <li class="flex w-full h-full relative">
-                        <h1 class="bg-white md:bg-orange-600 text-orange-600 md:text-white  font-bold text-center py-2 w-f w-full">BLOG</h1>
-                        <button class="bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 h-full font-bold border-2 px-2 z-10 border-white rounded-0 absolute right-0 md:hidden" @click="modal_menu=!modal_menu">
-                            <font-awesome-icon :icon="modal_menu ?  ['fas', 'bars']:['fas', 'x']" />
-                        </button>
-                    </li>
-                    <li class="mt-auto bg-white dark:bg-[#091a28]">
-                        <ul :class=" (modal_menu ? 'hidden':'') + ' md:block ' ">
-                            <li>
-                                <button class="dark:bg-transparent dark:border-0 dark:hover:bg-gray-900 dark:text-orange-500 bg-white dark:bg-gray-200  hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 border-2 border-white rounded-0 w-full">
-                                    Tag 22
-                                </button>
-                            </li>
-                            <li>
-                                <button class="dark:bg-transparent dark:border-0 dark:hover:bg-gray-900 dark:text-orange-500 bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 border-2 border-white rounded-0 w-full">
-                                    Tag 22
-                                </button>
-                            </li>
-                            <li>
-                                <button class="dark:bg-transparent dark:border-0 dark:hover:bg-gray-900 dark:text-orange-500 bg-white hover:bg-gray-100 text-gray-800 hover:text-orange-500 font-bold py-2 px-4 border-2 border-white rounded-0 w-full">
-                                    Tag 22
-                                </button>
-                            </li>
-                            <li><hr></li>
-                            <li class="mt-4">
-                                <ColorMode />
-                            </li>
-                        </ul>
-                    </li>
-                    
-                </ul>
-            </article>
-            <article class="w-full mt-10 md:mt-0 relative">
-                <div class=" top-0 z-10">
-                    <SearchBar />
-                </div>
-                <hr>        
-                <div class="my-5 grid grid-cols-3 gap-4" style="min-height: 30%;" >
-                    <div class="p-2 row-span-4 border border-red-300 bg-cover bg-center bg-no-repeat dark:grayscale" style="background-image: url(_nuxt/assets/img/solider.jpg);"></div>
-                    <div class="p-2 row-span-2 border border-red-300 bg-cover bg-center bg-no-repeat" style="background-image: url(_nuxt/assets/img/view.jpg);"></div>
-                    <div class="p-2 border border-red-300">
-                        <h2>4. title</h2>
-                    </div>
-                    <div class="p-2 border border-red-300">
-                        <h2>5. title</h2>
-                    </div>
-                    <div class="p-2 row-span-2 border border-red-300 bg-cover bg-center bg-no-repeat" style="background-image: url(_nuxt/assets/img/pc.jpg);"></div>
-                    <div class="p-2 border border-red-300">5.title</div>
-                    <div class="p-2 border border-red-300">6.title</div>
-                </div>      
-                <div class="my-5 bg-cover bg-bottom bg-no-repeat min-h-[40%] relative" style="background-image: url(_nuxt/assets/anim/jp.gif);">
-                    <div class="absolute flex m-auto h-full w-full">
-                        <div class="m-auto justify-center text-center">
-                            <h1 class="text-orange-600 font-bold text-8xl">Start Reading</h1>
-                            <button class="text-gray-400 hover:text-gray-500 text-6xl mt-2 hover:pt-3 ">
-                                <font-awesome-icon :icon="['fas', 'circle-down']"/>
-                            </button>
-                        </div>
-                    </div>
-                </div>  
-            </article>
-            <article class="w-full hidden md:block md:w-4/12 absolute md:static border-0 border-e border-gray-100">
-                <h1 class="bg-white md:bg-orange-600 text-orange-600 md:text-white  font-bold text-center py-2 w-f w-full">Trends</h1>
-            </article>
-        </section>
-    </section>
-    
+<header class="flex py-2">
+    <article class="w-4/12 my-auto ms-2"><h1 class="text-gray-700 my-auto">./blog @f4rukseker</h1></article>
+    <article class="w-full"><SearchBar /></article>
+    <article class="w-4/12 text-center my-auto"><button class="border border-gray-600 bg-gray-900 px-2 py-1 rounded font-bold text-white">./SSH</button></article>
+</header>
+<div class="flex">
+    <code class="min-w-max ms-2"><span class="bg-gray-600 p-1 rounded font-semibold text-white">f4ruk@blog:</span> dump all blog context >> .</code>
+    <hr class="w-full ms-2 my-auto">
+</div>
+<section class="flex mx-0 md:mx-2 lg:mx-3">
+    <article class="w-4/12">heade</article>
+    <article class="w-full border ">heade</article>
+</section>
 </template>
 
 <script setup>

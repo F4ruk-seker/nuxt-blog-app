34b8dd75ee1cea1a9ab53aff203761ee141ee3d1
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-04-17 18:15:19 +0300

    add Expend menu

--- pages/index.vue
+++ pages/index.vue
@@ -15,11 +15,19 @@
         </article>
         <article :class="!expend_search_bar ? 'min-w-max md:w-4/12 m-auto flex text-center':'hidden'">
             <ul class="list-none justify-center m-auto">
-                <li class="mx-2">
+                <li class="mx-2 hidden md:block">
                     <button class="border border-gray-600 bg-gray-900 px-2 py-1 rounded font-bold text-white shadow-sm">
                         <span class="hidden md:block font-[Ubuntu] font-normal">./SSH</span>
                         <span class="block md:hidden">
                             <font-awesome-icon :icon="['fas', 'user']" /> 
+                        </span>
+                    </button>
+                </li>
+                <li class="mx-2 block md:hidden">
+                    <button class="border border-gray-600 bg-gray-900 px-2 py-1 rounded font-bold text-white shadow-sm" @click="expend_mobil_menu=true">
+                        <span class="hidden md:block font-[Ubuntu] font-normal">./SsSH</span>
+                        <span class="block md:hidden">
+                            <font-awesome-icon :icon="['fas', 'bars']" /> 
                         </span>
                     </button>
                 </li>
@@ -33,8 +41,8 @@
 </article>
 </header>
 <section class="absolute w-full h-screen top-0 pt-16 flex overflow-y-scroll scrollbar-pars ">
-    <article class="w-3/12 h-full sticky top-0">
-        <MenuAsideMenu  />
+    <article :class="'w-full md:w-3/12 h-full top-0 z-20 bg-white dark:bg-gray-900 fixed md:sticky ' + (expend_mobil_menu ? '': 'hidden md:block')">
+        <MenuAsideMenu  @close_menu="expend_mobil_menu=!expend_mobil_menu"/>
     </article>
     <article class="w-full mx-0 md:mx-2" >
         <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
@@ -74,5 +82,6 @@
 import ColorMode from '@/components/theme/ColorMode.vue';
 
 const expend_search_bar = ref(false)
+const expend_mobil_menu = ref(false)
 // + (modal_menu ? 'show':'hidden')"
 </script>

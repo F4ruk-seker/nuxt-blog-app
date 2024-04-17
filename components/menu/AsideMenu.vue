82d7b7802ea7c5642f2842b259fd5c362f3d9f65
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-04-17 19:01:59 +0300

    add a tag selector with Checkbox

--- components/menu/AsideMenu.vue
+++ components/menu/AsideMenu.vue
@@ -1,12 +1,30 @@
 <template>
 <aside class="text-center">
-<ul class="list-none">
-    <li class="text-right mx-2 flex md:hidden mt-3">
+<ul class="list-none text-start mx-2">
+    <li class="text-right flex md:hidden mt-3">
         <h1 class="text-gray-700 my-auto font-semibold w-full text-left ">./blog @f4rukseker</h1>
         <button class="border border-gray-600 bg-gray-900 px-2 py-1 rounded font-bold text-white shadow-sm" @click="$emit('close_menu')">
             <font-awesome-icon :icon="['fas', 'x']" />
         </button>
     </li>
+    <li class="my-2">
+        <hr>
+    </li>
+    <li class="border border-orange-500 rounded p-1 mb-2">
+        <strong>xd</strong>
+    </li>
+    <li class="w-full flex mb-2">
+        <!--has-[:checked]-->
+        <label class="min-w-full p-1 rounded border border-orange-500 hover:border-orange-600 has-[:checked]:bg-orange-600 has-[:checked]:text-white">
+            <input class="hidden" type="checkbox" name="todo"> 
+            <strong class="">xd</strong>
+        </label>
+    </li>
+
+    <li>
+      
+    </li>
+    
 </ul>
 </aside>
 </template>

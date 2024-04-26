f656307c5b050060fc1ec115768d2c31af9d30bb
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-04-26 19:09:20 +0300

    text context-manages added to header menu

--- components/HeaderDetailMenu.vue
+++ components/HeaderDetailMenu.vue
@@ -1,16 +1,43 @@
 <template>
-<header class="fixed w-full top-0 z-10 bg-white dark:bg-gray-900">
-    <nav class="flex py-2 sticky top-0 justify-between">
-        <article class="min-w-max my-auto ms-2 me-2">
+<header class="fixed w-full top-0 z-10 bg-white dark:bg-gray-900 border-b border-orange-400 ">
+    <nav class="flex py-2 sticky top-0">
+        <article class="min-w-max md:w-full my-auto mx-4">
             <a href="/" class="">
                 <font-awesome-icon :icon="['fas', 'blog']" />
             </a>
         </article>
-        <article class="w-full">
-            V
-            X
+        <article class="w-full text-center flex justify-between">
+            <button class="min-w-max" @click="$emit('changeFont')">
+                <font-awesome-icon :icon="['fas', 'font']" />
+                <span class="font-bold">: {{ fontFamilyName }}</span>
+            </button>
+            <button class="min-w-max" @click="$emit('fontSize')">
+                <font-awesome-icon :icon="['fas', 't']" />
+                <span class="font-bold">: {{ fontSize }}</span>
+            </button>
+            <button class="min-w-max" @click="$emit('fontWeight')">
+                <font-awesome-icon :icon="['fas', 'bold']" />
+                <span class="font-bold">: {{ fontWeight }}</span>
+            </button>
         </article>
-        <article class="w-full">x</article>
+        <article class="min-w-max md:w-full text-center mx-4 md:mx-0 font-bold">Dark</article>
     </nav>
 </header>
 </template>
+
+<script setup>
+
+const props = defineProps({
+    fontFamilyName:String,
+    fontSize:String,
+    fontWeight:String
+})
+
+const emit = defineEmits([
+    'changeFont',
+    'fontSize',
+    'fontWeight'
+])
+
+
+</script>
--- pages/[slug].vue
+++ pages/[slug].vue
@@ -1,21 +1,17 @@
 <template>
-<HeaderDetailMenu />
+<HeaderDetailMenu 
+    :fontFamilyName="fontList[selectedFontIndex].name"
+    @changeFont="changeFont"
+    :fontSize="getFontSize()"
+    @fontSize="fontSize++"
+    :fontWeight="getFontWeight()"
+    @fontWeight="fontWeight++"
+    />
 
 <section class="absolute w-full h-screen top-0 pt-16 overflow-y-scroll overflow-x-hidden scrollbar-pars ">
     
-    <button @click="changeFont">
-        fontChange
-    </button>
-    <br>
-    <button @click="fontSize++">
-        Size
-    </button>
-    <br>
-    <button @click="fontWeight++">
-        bolder
-    </button>
 
-    <article :class="`container m-auto  ${fontList[selectedFontIndex].font} ${getFontSize()} ${getFontWeight()}`">
+    <article :class="`container m-auto px-2  ${fontList[selectedFontIndex].font} ${getFontSize()} font-${getFontWeight()}`">
         
         The standard Lorem Ipsum passage, used since the 1500s
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -31,12 +27,12 @@
 
         1914 translation by H. Rackham
         "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
-    <hr>
+    
+</article>
+<hr>
     <span class="text-lg">
         tex : {{  context }}
-    </span>    
-</article>
-
+    </span>  
 </section>
 
 
@@ -102,11 +98,11 @@ const fontWeight = ref(1)
 function getFontWeight() {
     switch(fontWeight.value) {
         case 0:
-            return 'font-normal'
+            return 'normal'
         case 1:
-            return 'font-bold'
+            return 'bold'
         case 2:
-            return 'font-extrabold'
+            return 'extrabold'
         default:
             fontWeight.value = 0        
             getFontWeight()
